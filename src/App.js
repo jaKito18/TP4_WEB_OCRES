125075a6e64f8f3ff84bf4ff2e3cc2811a62e1a3
Jakob Zettel <jakobzettel@Jakobs-MacBook-Pro.local>
2020-10-24 16:42:30 +0200

    Starting construction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+
+function Profilebutton(props){
+  return(
+    <button>{props.name}</button>
+  )
+}
+
+class Profile extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+  render(){
+  return(
+    <div> 
+      <p>Prenom:</p> <p>Nom:</p> <p>Date de naissance:</p>
+      <button>Change style</button>
+    </div>
+    
+  )
+}
+}
 
 function App() {
+ let activeprofile =0;
+  const profileinfo = [
+    {
+      prenom: "Pierre",
+      nom: "Chartier",
+      birth: "23.09.1998"
+    },
+    {
+      prenom: "Martine",
+      nom: "Matterell",
+      birth: "23.09.1998"
+    },
+    {
+      prenom: "Camille",
+      nom: "Puissan",
+      birth: "23.09.1998"
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        {profileinfo.map((profil, index)=>
+        <Profilebutton name={profil.prenom} key={index}></Profilebutton>)}
       </header>
+      <body>
+          <Profile prenom={profileinfo[activeprofile].prenom}></Profile>
+          <Profile nom={profileinfo[activeprofile].nom}></Profile>
+          <Profile birth={profileinfo[activeprofile].birth}></Profile>
+      </body>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/script.js
@@ -0,0 +1,35 @@
+
+
+// const profils = [
+//   {
+//     nom: Pierre
+//   },
+//   {
+//     nom: Martine
+//   },
+//   {
+//     nom: Camille
+//   }
+// ];
+
+// class Profile extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       prename=new Prename();
+//       surname=new Surname();
+//       birthdate=new Date();
+//     }
+//   }
+
+// class Profile extends React.Component {
+//   render() {
+//     return (
+//       <div className="Profile">
+//         this.prename = prename;
+//         this.lastname = lastname;
+//         this.birthday;
+//         this.pic = 
+//       </div>
+//     );
+//   }
